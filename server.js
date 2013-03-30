ad271e808f9b4e6375b9c8319eaa4ecf78f004ee
Joey <joey@eproject-inc.com>
2013-03-30 12:54:02 -0700

    test

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello.txt', function(req, res){
+app.get('/hello', function(req, res){
   res.send('Hello World');
 });
+
+app.get('/', function(req, res){
+  res.send('Sup');
+});
+
 
 app.listen(3000);
 console.log('Listening on port 3000');
