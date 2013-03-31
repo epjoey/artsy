ea69fe1b172be5727cce89cbfe7bbc1282d5687b
Joey <joey@eproject-inc.com>
2013-03-30 20:53:21 -0700

    spaces

--- server.js
+++ server.js
@@ -3,28 +3,28 @@ var app = express();
 var Backbone = require('backbone');
 
 var AppRouter = Backbone.Router.extend({
-	routes: {
-	  "/" : "all",
-	  "/subpage" : "subpage"
-	},
-	all: function() {
-		return 'all';
-	},
-	subpage: function() {
-		return 'subpage';
-	}
+    routes: {
+        "/" : "all",
+        "/subpage" : "subpage"
+    },
+    all: function() {
+        return 'all';
+    },
+    subpage: function() {
+        return 'subpage';
+    }
 });
 
 app.use(function(req, res){
-  	var router = new AppRouter(),
-		route = router.routes[req.url],
-		response = null;
-	if (!route) {
-		response = 'not found';
-	} else {
-		response = router[route]();	
-	}  
-  	res.send(response);
+    var router = new AppRouter(),
+        route = router.routes[req.url],
+        response = null;
+    if (!route) {
+        response = 'not found';
+    } else {
+        response = router[route](); 
+    }  
+    res.send(response);
 });
 
 app.listen(3000);
