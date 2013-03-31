b01e19dcbcdde5f345d2f43b9c97f740d1d7e032
Joey <joey@eproject-inc.com>
2013-03-30 20:42:06 -0700

    routing from express

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 var express = require('express');
 var app = express();
+var Backbone = require('backbone');
 
-app.get('/hello', function(req, res){
-  res.send('Hello World');
+var AppRouter = Backbone.Router.extend({
+	routes: {
+	  "/" : "all",
+	  "/subpage" : "subpage"
+	},
+	all: function() {
+		return 'all';
+	},
+	subpage: function() {
+		return 'subpage';
+	}
 });
 
-app.get('/', function(req, res){
-  res.send('Sup');
+app.use(function(req, res){
+  	var router = new AppRouter(),
+		route = router.routes[req.url],
+		response = null;
+	if (!route) {
+		response = 'not found';
+	} else {
+		response = router[route]();	
+	}  
+  	res.send(response);
 });
-
 
 app.listen(3000);
 console.log('Listening on port 3000');
