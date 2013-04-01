1117d84d205de45a80cf121b8d7e36b57bcbf7d4
Joey <joey@eproject-inc.com>
2013-03-31 22:28:20 -0700

    express router more simple

--- server.js
+++ server.js
@@ -20,13 +20,26 @@ var AppRouter = Backbone.Router.extend({
     }
 });
 
-var router = new AppRouter();
-_.each(router.routes, function(callback, route) {
+//var router = new AppRouter();
+// _.each(router.routes, function(callback, route) {
+//     app.get(route, function(req, res){
+//         res.send(router[callback](req.params));
+//     });
+// });
+
+var routes = {
+    '/' : 'index',
+    '/about' : 'about',
+    '/classes/:class?' : 'classes',
+    '/contact' : 'contact'
+};
+
+_.each(routes, function(controllerName, route) {
     app.get(route, function(req, res){
-        res.send(router[callback](req.params));
+        var controller = new controllerName(req.params);
+        res.send(controller.getTemplate());
     });
 });
-
 
 app.listen(3000);
 console.log('Listening on port 3000');
