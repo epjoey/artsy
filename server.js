bc7d5648cc248671ca8679a374e7d470756dcf16
Joey <joey@eproject-inc.com>
2013-04-07 00:52:00 -0700

    removed unneeded controllers

--- server.js
+++ server.js
@@ -1,42 +1,11 @@
 var express = require('express')
-  , app = express()
-  //, _ = require('underscore')._
-  //, fs = require('fs');
-  //, requirejs = require('requirejs');
-
-
-// requirejs.config({
-//     //Pass the top-level main.js/index.js require
-//     //function to requirejs so that node modules
-//     //are loaded relative to the top-level JS file.
-//     nodeRequire: require
-// });
+  , app = express();
 
 app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/'));
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
-
-// requirejs(['lib/jade-compile'], function(jadeCompile) {
-//   //compile all public templates from 'views/templates' to client-ready javascript functions in 'public/templates'
-//   jadeCompile('views/templates', 'public/templates', {
-//     client: true,
-//     compileDebug: true
-//   });
-// });
-
-
-
-// requirejs(["routes"], function(routes){
-//   _.each(routes.get, function(controller, route){
-//     app.get('/' + route, function(req, res){
-//       requirejs(['controllers/' + controller], function(c) {
-//         res.render(c.getTemplateName(req), c.getTemplateData(req));
-//       });
-//     });    
-//   });
-// });
 
 app.get('/', function(req, res){
   var tpl = req.query.pushstate ? 'content/index-page' : 'index-page';
